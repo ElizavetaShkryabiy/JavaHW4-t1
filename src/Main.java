c093bac2e0fdc94334ff4208a5f9cb8001fa0ebd
Elizaveta Shkryabiy <evshkryabiy@gmail.com>
2021-07-30 19:41:12 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println("Вам начислено миль:");
+        System.out.println(miles);
+
+
+    }
+}
